7562619b152489873ddad4af2390dc2c83a61fac
thismali <mali@mail.umkc.edu>
2015-10-14 01:44:10 -0500

    SpeechToText

--- app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
+++ app/src/main/java/com/umkc/hastimal/nursetogo/MainActivity.java
@@ -1,16 +1,23 @@
 package com.umkc.hastimal.nursetogo;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.URI;
 import java.net.UnknownHostException;
+import java.util.Locale;
+
 import android.os.AsyncTask;
 import android.app.Activity;
 import android.util.Log;
@@ -20,15 +27,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+import com.ibm.cio.dto.QueryResult;
+import com.ibm.cio.watsonsdk.SpeechDelegate;
+import com.ibm.cio.watsonsdk.TextToSpeech;
+
+
+public class MainActivity extends Activity implements SpeechDelegate {
 
     TextView textResponse;
     EditText editTextAddress, editTextPort;
     Button buttonConnect, buttonClear,buttonBackward,buttonForward,buttonLeft,buttonRight,buttonHeadUp,buttonHeadDown;
-    ImageButton buttonStop;
+    ImageButton recordButton;
     String command;
     Boolean checkupdate=false;
+
+    public final static String username = "********************************";
+    public final static String password = "********************************";
+    public final static String apiURL = "https://stream.watsonplatform.net/speech-to-text-beta/api";
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,16 +66,18 @@ public class MainActivity extends Activity {
         buttonRight=(Button) findViewById(R.id.right);
         buttonHeadUp=(Button) findViewById(R.id.headUp);
         buttonHeadDown=(Button) findViewById(R.id.headDown);
-        buttonStop=(ImageButton) findViewById(R.id.stop);
+        recordButton =(ImageButton) findViewById(R.id.recordButton);
 
-        buttonStop.setOnClickListener(new OnClickListener(){
+        recordButton.setOnClickListener(new OnClickListener(){
 
             @Override
             public void onClick(View arg0) {
                 Log.d(".....Stop Button","...pressed");
-                // TODO Auto-generated method stub
-                command="stop";
+
+                //command="stop";
                 checkupdate=true;
+
+                Speak();
             }
 
         });
@@ -147,6 +169,45 @@ public class MainActivity extends Activity {
                             Integer.parseInt(editTextPort.getText().toString()));
                     myClientTask.execute();
                 }};
+
+    public void Speak()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Start speaking!");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "speech not supported :(",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onOpen() {
+        // the  connection to the STT service is successfully opened
+    }
+
+    public void onError(String error) {
+        // error interacting with the STT service
+    }
+
+    public void onClose(int code, String reason, boolean remote) {
+        // the connection with the STT service was just closed
+    }
+
+    public void onMessage(String message) {
+        // a message comes from the STT service with recognition results
+    }
+
+    @Override
+    public void receivedMessage(int i, QueryResult queryResult) {
+
+    }
+
     public class MyClientTask extends AsyncTask<Void, Void, Void> {
 
         String dstAddress;
@@ -209,6 +270,10 @@ public class MainActivity extends Activity {
             textResponse.setText(response);
             super.onPostExecute(result);
         }
+
+
+
+
 
     }
 }
